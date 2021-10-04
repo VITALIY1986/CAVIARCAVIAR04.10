e4b8b631922d789a8553ecf197d0ac35334e1c27
VITALIY1986 <staslego1986@gmail.com>
2021-10-04 22:07:09 +0300

    asd

--- pages/gift/[slug].js
+++ pages/gift/[slug].js
@@ -8,10 +8,28 @@ import giftboxList from "../../lib/gift.json"
 
 
 
-
 const Productpage = (props ) => {
-  console.log(props.gift[0].fields.id)
- 
+  
+  const [orderOption, setOrderOption] = useState({
+    name: props.option[0].fields.label,
+    price: props.option[0].fields.price
+  })
+  const changeOrderOption = (n, p) => {
+    setOrderOption({
+      name:n,
+      price:p
+    })
+  }
+
+  const [orderQty, setOrderQty] = useState(1)
+  const increment = () => {
+    setOrderQty(orderQty+1)
+    return false
+  }
+  const dectrement = () => {
+    orderQty>1 ? setOrderQty(orderQty-1) : null
+    return false
+  }
 
   return (
     <div className=" mx-auto  ">
@@ -26,7 +44,9 @@ const Productpage = (props ) => {
       <section className="container overflow-x-hidden mx-auto text-center flex flex-wrap w-full items-center mt-20" >
         <div className="w-full md:w-2/4">
             <h1 className="text-7xl text-white"></h1>
-           
+            <div className="container ">
+                <img className="w-full " src= {props.gift[0].fields.image.fields.file.url.toString()} alt=""  />
+           </div>
         </div>
         <div className="md:w-2/4">
              <h1 className=" text-white uppercase text-4xl leading-10 mt-4">
@@ -35,24 +55,55 @@ const Productpage = (props ) => {
                <img className="inline align-middle ml-2 w-2" src="/static/image 2.svg" alt="" />
             </h1>
         
-         
+            <p className="uppercase  mt-3 text-xl amber_caviar_cart -m-3" >
+             {props.gift[0].fields.type}
+            </p>
        
         <div className="text-white mt-10 text-center px-10 ">
-             
+          <p className=" mb-10 text-justify text-xs md:text-md">
+            {props.gift[0].fields.description}
+          </p>      
         </div>
        
-        
+        <div className="mx-2 boxed-option grid grid-cols-2 gap-4">
+            
+    
+                  {props.option.map ((i, idx) => {
+                    return (
+                      <label key={idx}>
+                        <input className="boxed-radio"  id="" type="radio" checked={i.fields.label==orderOption ? true: false}  name="Options" value={i.fields.label} 
+                          onChange={()=>{changeOrderOption(i.fields.label, i.fields.price)}}    />
+                        <span className="option-value-name ">
+                          <span >{i.fields.label}</span>
+                          <span className="option-serves">{i.fields.serves}</span>
+                          <span className="option-price">{i.fields.price}</span>
+                        </span>
+                      </label>
+                    )})}
+               
+             
+        </div>
 
-        
+        <div>
+          <button className="text-white uppercase" onClick={()=>{dectrement()}}>-</button>
+          <input className="quantity-input__screen text-white rounded-sm border bg-grey border-gold w-12 h-8 mt-10 mb-8 mx-3 pl-1" type="text" value={orderQty} />
+          <button className="text-white uppercase" onClick={()=>{increment()}}>+</button>
+        </div>
 
-        
+        <div className="mt-3 mx-2 ">
+          <button className="snipcart-add-item text-white uppercase bg-gold p-4 w-full rounded-sm"
+            data-item-id={props.gift[0].fields.id}
+            data-item-name={props.gift[0].fields.name + "-" }
+            data-item-price={Number(orderOption.price)*Number(orderQty)}
+            data-item-url={"/product/" + props.gift[0].fields.id}
+            data-item-image={props.gift[0].fields.image.fields.file.url}>Add to Cart <span>${Number(orderOption.price)*Number(orderQty)}</span></button>
+        </div>
         </div>
       </section>
       <Footer />
     </div>
   )
 }
-
 
 Productpage.getInitialProps = async function({query}) {
 
@@ -66,10 +117,13 @@ Productpage.getInitialProps = async function({query}) {
    let result = res.items.filter(obj=>{
      return obj.fields.id === query.slug
    })
-   
+   let optionres = opt.items.filter(o=>{
+    return o.fields.id === query.slug
+  })
     return {
-    
+    option:optionres,
       gift:result
     };
   };
+
 export default Productpage
